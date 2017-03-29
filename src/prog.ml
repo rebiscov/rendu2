2a199002bcf76fac4b5bb75f25e6daee6463b2a8
Julien <julien.du-crest@ens-lyon.fr>
2017-03-29 11:41:59 +0200

    nice commit

--- src/prog.ml
+++ src/prog.ml
@@ -1,3 +1,6 @@
+open Printf
+
+
 type ident = string
 
 
@@ -9,4 +12,20 @@ type prog =
 	| Plus of prog*prog      (* /!\ Plus, Mult and Print must fail if the prog is a function of arity > 1 *)
 	| Mult of prog*prog
 	| Print of prog
-        | Var of ident;;
+    | Id of ident;;
+
+let rec print_prog p = 
+	match p with
+	| Let(id,p1,p2) -> 	printf "Let(%s," id;
+						print_prog p1;
+						printf ",";
+						print_prog p2;
+						printf ")"
+	| Value(n)		->	printf "Value(%d)" n
+	| Id(id)		-> 	printf "Id(%s)" id
+	| Plus(p1,p2)	-> 	printf "Plus(";
+						print_prog p1;
+						printf ",";
+						print_prog p2;
+						printf ")"
+	| _ -> printf "$not impl$";;
