d64774c06606cf1438c97b98eb0e2b51a64b3e1d
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-29 11:31:35 +0200

    Adding prog

---
+++ src/prog.ml
@@ -0,0 +1,12 @@
+type ident = string
+
+
+type prog = 
+	| App of prog*prog
+	| Fun of ident*prog
+	| Let of ident*prog*prog
+	| Value of int
+	| Plus of prog*prog      (* /!\ Plus, Mult and Print must fail if the prog is a function of arity > 1 *)
+	| Mult of prog*prog
+	| Print of prog
+        | Var of ident;;
