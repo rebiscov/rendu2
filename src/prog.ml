bd280a0f1efea1be11b592b336a001fb565500cd
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-29 10:51:53 +0200

    Fixing prog.ml

--- src/prog.ml
+++ src/prog.ml
@@ -1,8 +1,9 @@
-type var = string
+type ident = string
+
 
 type prog = 
 	| App of prog*prog
-	| Fun of var*prog
+	| Fun of ident*prog
 	| Let of ident*prog*prog
 	| Value of int
 	| Plus of prog*prog      (* /!\ Plus, Mult and Print must fail if the prog is a function of arity > 1 *)
