83d45b731a4da5081155e0415c3872fc149b5ea6
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-29 19:30:33 +0200

    interpreter handles + and variables assignations

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -9,8 +9,8 @@ let rec interpreter = fun prg ->
   | Let(name, prg1, prg2) ->begin try
                              Hashtbl.add env name (interpreter prg1);
                              let prg' = interpreter prg2 in Hashtbl.remove env name; prg'
-                           with 
-                           | Not_found -> printf "The key %s is not present in the hashtable" name; failwith("Not Found")
+                           with
+                           | Not_found -> printf "The key %s is not present in the hashtable" name; failwith("Not found")
                            | _ -> failwith("Unknown error in interpreter: Let definition") end
                           
   | Plus(prg1, prg2) -> let prg1' = interpreter prg1 in
@@ -18,7 +18,13 @@ let rec interpreter = fun prg ->
                             match (prg1', prg2') with
                             | (Value(a), Value(b)) -> Value(a+b)
                             | _ -> failwith("Not a valid addition") end
+                                                      
   | Value(a) -> Value(a)
-  | Id(ident) -> Hashtbl.find env ident
+  | Id(ident) -> begin
+     try
+       Hashtbl.find env ident
+     with
+     | Not_found -> printf "The key %s is not present in the hashtable" ident; failwith("Not found")
+     | _ -> failwith("Unknown error in interpreter: Id") end
+                     
   | _ -> failwith("Not supported yet");;
-
