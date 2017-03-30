4b330f37d4f223c25b4fda4828ae32a08a479ea9
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-30 13:15:55 +0200

    _

--- src/prog.ml
+++ src/prog.ml
@@ -5,14 +5,9 @@ type ident = string
 
 
 type prog = 
-<<<<<<< HEAD
-	| App of prog*prog (* Application of a function *)
-	| Fun of ident*prog (* Declaration of a function *)
-=======
 	| App of prog*prog
 	| Fun of ident*prog
 	| Recfun of ident*prog
->>>>>>> fa1673849102c50470bfa98783695bd71ea1ea66
 	| Let of ident*prog*prog
 	| Value of int
 	| Plus of prog*prog      (* /!\ Plus, Mult and Print must fail if the prog is a function of arity > 1 *)
