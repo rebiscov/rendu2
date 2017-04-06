1c64cd99882e15932fb9c3b43f37df534bfc0c66
Julien <julien.du-crest@ens-lyon.fr>
2017-04-06 15:07:45 +0200

    modified main for debug

--- src/main.ml
+++ src/main.ml
@@ -10,14 +10,26 @@ let parse () = Parser.main Lexer.token lexbuf;;
 (* we need to build an arg-parser *)
   
 let main () =
-  let inter = ref false in
-  for i = 0 to Array.length Sys.argv -1 do
-    if Sys.argv.(i) = "--interpreter" then inter := true
-    else ()
-  done;
-  if !inter then let prog = parse() in print_prog prog; print_prog (interpreter prog env)
-  else let prog = parse() in
-       print_prog prog;;
+	let inter = ref false in
+	let debug = ref false in
+	for i = 0 to Array.length Sys.argv -1 do
 
-  main();;
+		if Sys.argv.(i) = "--interpreter" || Sys.argv.(i) = "-i" then 
+			inter := true
+
+		else if Sys.argv.(i) = "-debug" || Sys.argv.(i) = "-d" then 
+			debug := true
+		
+		else ()
+	done;
+	if !inter then 
+		let prog = parse() in 
+		print_prog prog; 
+		print_prog (interpreter prog env)
+	else 
+		let prog = parse() in
+		print_prog prog;;
+
+
+main();;
     
--- src/prog.ml
+++ src/prog.ml
@@ -5,29 +5,42 @@ type ident = string
 
 
 type prog =
+	(* do nothing type ... ???why do we need it???*)
 	| Unit
-	| App of prog*prog
+	(* function definition *)
+	| Let of ident*prog*prog
 	| Fun of ident*prog
 	| Recfun of ident*prog
-	| Let of ident*prog*prog
-	| Value of int
-	| Plus of prog*prog      (* /!\ Plus, Mult and Print must fail if the prog is a function of arity > 1 *)
-	| Mult of prog*prog
-	| Print of prog
+	(* access *)	
 	| Id of ident;;
+	| Value of int
+	(* operators : must fail if the prog is a function of arity >= 1 *)	
+	| Plus of prog*prog     
+	| Mult of prog*prog			(* not parsed yet *)
+	| Minus of prog*prog		(* not parsed yet *)
+	(* fun application *)
+	| App of prog*prog
+	(* toolkit *)
+	| Print of prog
+;;
 
+(* if i have time, can do a beauty printer of prog with line switches and indentation *)
+let print_tabs n = 
+	match n with
+	| 0 -> ()
+	| _ -> 	printf "  "; 
+			print_tabs (n-1)
+;;
+
+let bprint_prog p = 
+	printf "$not implemented$"
+;;
 
 let print_prog p = 
 let rec print_prog' p = 
 	match p with
+	| Unit			->	printf "Unit()"
 	| Let(id,p1,p2) -> 	printf "Let(%s," id;
-						print_prog' p1;
-						printf ",";
-						print_prog' p2;
-						printf ")"
-	| Value(n)		->	printf "Value(%d)" n
-	| Id(id)		-> 	printf "Id(%s)" id
-	| Plus(p1,p2)	-> 	printf "Plus(";
 						print_prog' p1;
 						printf ",";
 						print_prog' p2;
@@ -38,11 +51,31 @@ let rec print_prog' p =
 	| Recfun(id,p1) -> 	printf "Recfun(%s," id;
 						print_prog' p1;
 						printf ")"
+	| Id(id)		-> 	printf "Id(%s)" id
+	| Value(n)		->	printf "Value(%d)" n
+	| Plus(p1,p2)	-> 	printf "Plus(";
+						print_prog' p1;
+						printf ",";
+						print_prog' p2;
+						printf ")"
+	| Mult(p1,p2)	->	printf "Mult(";
+						print_prog' p1;
+						printf ",";
+						print_prog' p2;
+						printf ")"
+	| Minus(p1,p2)	->	printf "Minus(";
+						print_prog' p1;
+						printf ",";
+						print_prog' p2;
+						printf ")"
 	| App(p1,p2)	-> 	printf "App(";
 						print_prog' p1;
 						printf ",";
 						print_prog' p2;
 						printf")"
+	| Print(p1)		-> 	printf "Print(";
+						print_prog' p1;
+						printf ")"
 	| _ -> printf "$not impl$"
 in
 print_prog' p;
